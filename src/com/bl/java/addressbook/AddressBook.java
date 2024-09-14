9ab2ced5bc1f2651c92ab9d2a7e85f5188f53469
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-14 11:39:56 +0530

    Added: Welcome Message is added

---
+++ src/com/bl/java/addressbook/AddressBook.java
@@ -0,0 +1,10 @@
+package com.bl.java.addressbook;
+
+public class AddressBook {
+
+    public static void main(String[] args)
+    {
+        System.out.println("--- Welcome to Address Book ---");
+
+    }
+}
